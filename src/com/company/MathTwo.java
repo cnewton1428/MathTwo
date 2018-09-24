e0d897482a0125195d0de9ef359a27fe284e417f
Carina <cncnewton37@gmail.com>
2018-09-24 10:09:12 -0400

    Initial commit

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class MathTwo {
+
+    public static void main(String[] args) {
+
+	int mynumber;
+	double myothernumber;
+
+	mynumber = 2;
+	myothernumber = 1.7938;
+
+
+	System.out.println(mynumber);
+	System.out.println(myothernumber);
+
+
+    }
+}
